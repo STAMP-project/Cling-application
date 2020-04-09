78d070e021fb144c45f1b717e925e8d7fc0170f8
pderakhshanfar <pouria.derakhshanfar@gmail.com>
2020-04-09 15:12:41 +0200

    Fix subject-filtering bash file

--- scripts/main/subjects-filtering.sh
+++ scripts/main/subjects-filtering.sh
@@ -1,25 +1,25 @@
-# projects=("closure" "math" "lang" "time" "mockito")
-# prefixes=("com.google.javascript" "org.apache.commons.math" "org.apache.commons.lang" "org.joda.time" "org.mockito")
+projects=("closure" "math" "lang" "time" "mockito")
+prefixes=("com.google.javascript" "org.apache.commons.math" "org.apache.commons.lang" "org.joda.time" "org.mockito")
 
-# END=${#projects[@]}
+END=${#projects[@]}
 
 
-# for ((i=0;i<END;i++)); do
-#     case=${projects[$i]}
-#     prefix=${prefixes[$i]}
-#     CPEntriesDir="projects/$case/cp-entries.txt"
+for ((i=0;i<END;i++)); do
+    case=${projects[$i]}
+    prefix=${prefixes[$i]}
+    CPEntriesDir="projects/$case/cp-entries.txt"
 
-#     echo "Running coupling analysis on $case"
+    echo "Running coupling analysis on $case"
 
-#     if [ -f "$CPEntriesDir" ] && [ ! -z "$( cat $CPEntriesDir)" ]; then
-#         CPEntriesContent=$( cat $CPEntriesDir)
-#         preparedCPs=$( python scripts/python/reassemble-cps.py $CPEntriesContent "$case")
+    if [ -f "$CPEntriesDir" ] && [ ! -z "$( cat $CPEntriesDir)" ]; then
+        CPEntriesContent=$( cat $CPEntriesDir)
+        preparedCPs=$( python scripts/python/reassemble-cps.py $CPEntriesContent "$case")
 
-#         java -jar tools/coupling.jar "-project_prefix" $prefix "-project_cp" "$preparedCPs" "-out_dir" "temp-csvs/$case"
-#     else
-#       echo "could not find cp-entries file"
-#     fi
-# done
+        java -jar tools/coupling.jar "-project_prefix" $prefix "-project_cp" "$preparedCPs" "-out_dir" "temp-csvs/$case"
+    else
+      echo "could not find cp-entries file"
+    fi
+done
 
 
 # Merge CSV files
