84c4d8be7a605ab4860f36420b253330e6e2d0ef
pderakhshanfar <pouria.derakhshanfar@gmail.com>
2020-04-15 23:08:03 +0200

    Update RQ2 documentations

--- scripts/main/rq2-killed-mutants.sh
+++ scripts/main/rq2-killed-mutants.sh
@@ -1,15 +1,18 @@
-# Detect killed mutants by EvoSuite
+# Detect mutants killed by EvoSuite
 python scripts/python/mutation/find-mutants-killed-by-evosuite.py
 
-# Detect killed mutants by Cling
+# Detect mutants killed by Cling
 python scripts/python/mutation/find-mutants-killed-by-cling.py
 
+# Detect mutants killed by Cling but not by TE
 python scripts/python/mutation/cling-evoe.py
 
+# Detect mutants killed by Cling but not by TR
 python scripts/python/mutation/cling-evor.py
 
+# Detect mutants killed only by Cling
 python scripts/python/mutation/cling-evoe-evor.py
 
+# Save the final csv file which contains the information about the killed mutants by Cling
 Rscript data_analysis/r-scripts/killedM.r 
-
 python scripts/python/mutation/generate-final-mutants-table.py
