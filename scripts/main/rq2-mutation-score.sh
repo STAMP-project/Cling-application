dd90788bab609ada5b9aacab955f107d43946bdf
pderakhshanfar <pouria.derakhshanfar@gmail.com>
2020-04-15 15:11:56 +0200

    Update rq2 script

--- scripts/main/rq2-mutation-score.sh
+++ scripts/main/rq2-mutation-score.sh
@@ -1,19 +1,19 @@
 
 LIMIT=$1
 
-# echo "Set class separator to false"
-# . scripts/test/prepare_tests.sh
+echo "Set class separator to false"
+. scripts/test/prepare_tests.sh
 
-# # compile the generated tests
-# echo "compiling the generated tests"
-# . scripts/test/compile_tests.sh
+# compile the generated tests
+echo "compiling the generated tests"
+. scripts/test/compile_tests.sh
 
-# # detect and remove flaky tests from the generated test suites
-# echo "Detecting the flaky tests"
-# . scripts/test/detect_flaky_tests.sh
+# detect and remove flaky tests from the generated test suites
+echo "Detecting the flaky tests"
+. scripts/test/detect_flaky_tests.sh
 
-# echo "Re-compile the modified tests"
-# . scripts/test/compile_tests.sh
+echo "Re-compile the modified tests"
+. scripts/test/compile_tests.sh
 
 echo "Running pitest"
 . scripts/mutation/mutation-score-calculator.sh $LIMIT
