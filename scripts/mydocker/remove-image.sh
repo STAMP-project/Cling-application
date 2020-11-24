01c7fa6940292fdcd6ed8578b8856d417f16cbf9
pderakhshanfar <pouria.derakhshanfar@gmail.com>
2020-11-24 19:27:44 +0100

    Add mydocker scripts

---
+++ scripts/mydocker/remove-image.sh
@@ -0,0 +1,6 @@
+# Stop the container from previous experiments
+mydocker stop cling-application-container
+# Remove old containers
+mydocker rm cling-application-container
+# Remove previous mydocker image
+# mydocker rmi cling-application
