c18c013096deb34bf7ae2fee220cb3a2e7f14dbb
Pouria Derakhshanfar <pouria.derakhshanfar@gmail.com>
2022-03-05 10:25:13 +0100

    reorder subjects according to their execution id

--- subject_generator/generate.py
+++ subject_generator/generate.py
@@ -4,9 +4,10 @@ import csv
 dir_path = os.path.dirname(os.path.realpath(__file__))
 csvDir = os.path.join(dir_path,"selected-class-pairs.csv")
 
-tools = ["cling","evosuite-callee5","evosuite-caller5","randoop-callee5","randoop-caller5"]
+# tools = ["cling","evosuite-callee5","evosuite-caller5","randoop"]
+tools = ["randoop"]
 
-rounds = range(1, 21)
+rounds = range(1, 6)
 
 fieldnames = ['tool', 'execution_id', 'project', 'caller_class', 'callee_class', 'type']
 
@@ -17,16 +18,20 @@ writer = csv.writer(csvFile)
 
 writer.writerow(fieldnames)
 
-with open(csvDir, 'r') as _filehandler:
-    csv_file_reader = csv.DictReader(_filehandler)
-    for row in csv_file_reader:
-        case=row['case']
-        caller=row['caller']
-        callee=row['callee']
-        type=row['type']
-        for execution_id in rounds:
+
+for execution_id in rounds:
+    with open(csvDir, 'r') as _filehandler:
+        csv_file_reader = csv.DictReader(_filehandler)
+        print(execution_id)
+        for row in csv_file_reader:
+            case=row['case']
+            print(case)
+            caller=row['caller']
+            callee=row['callee']
+            type=row['type']
             for tool in tools:
                 tempRow = [tool, execution_id, case, caller, callee, type]
+                print(tempRow)
                 writer.writerow(tempRow)
 
 
