3c2d8512819138e0bad048b8f24f79a7b3009be6
Pouria Derakhshanfar <pouria.derakhshanfar@gmail.com>
2022-03-28 17:17:03 +0200

    Prepare script for completing PIT execution

--- scripts/mutation/parsing.sh
+++ scripts/mutation/parsing.sh
@@ -13,7 +13,7 @@ resultDir=$9
 outDir=${10}
 ##
 
-
+echo "Parsing $pid"
 ## wait for the process to finish
 while kill -0 "$pid"; do
     sleep 1
@@ -26,18 +26,68 @@ if [[ "$exists" == "TRUE" ]]
 then
     echo "A problem in the PIT execution has been found."
     echo "Fixing the problem ..."
-    failedClass=$(python scripts/python/mutation/detect_failing_class.py "$logFile" "did not pass without mutation")
-    failedTest=$(python scripts/python/mutation/detect_failing_test.py "$logFile" "did not pass without mutation")
 
-    # Add @Ignore to failing tests
-    for mainTest in `find $resultDir -name "*_ESTest.java" -type f`; do
-          echo "Add ignore to $failedTest in $mainTest"
-          java -jar tools/IgnoreAdder.jar $mainTest "$failedTest"
-          CPEntriesDir="projects/$project/cp-entries.txt"
-          CPEntriesContent=$( cat $CPEntriesDir)
-          preparedCPs=$( python scripts/python/reassemble-cps.py $CPEntriesContent "$project")
-          javac -cp "$preparedCPs:$resultDir:$(cat libs/test_execution/classpath.txt)" $mainTest
-    done
+    if [[ "$tool" == "randoop" ]]; then
+        counter=1
+        oldTestClass=""
+        while :
+        do
+            failedClass=$(python scripts/python/mutation/detect_failing_class_randoop.py "$logFile" "did not pass without mutation" $counter)
+            if [[ "$failedClass" == "" ]]; then
+                echo "No more failing test"
+                 if [[ "$oldTestClass" != "" ]]; then
+                    # Compile old test
+                    CPEntriesDir="projects/$project/cp-entries.txt"
+                    CPEntriesContent=$( cat $CPEntriesDir)
+                    preparedCPs=$( python scripts/python/reassemble-cps.py $CPEntriesContent "$project")
+                    echo "Compiled test $oldTestClass"
+                    javac -cp "$preparedCPs:$resultDir:$(cat libs/test_execution/classpath.txt)" $oldTestClass
+                else
+                    echo "did not compile the sub class: "
+                fi
+                break
+            fi
+            failedClass="$resultDir/$failedClass.java"
+            failedTest=$(python scripts/python/mutation/detect_failing_test_randoop.py "$logFile" "did not pass without mutation" $counter)
+            echo "Add ignore to $failedTest in $failedClass"
+            java -jar tools/IgnoreAdder.jar "$failedClass" "$failedTest"
+
+            if [[ "$failedClass" != "$oldTestClass" ]]; then 
+                if [[ "$oldTestClass" != "" ]]; then
+                    # Compile old test
+                    CPEntriesDir="projects/$project/cp-entries.txt"
+                    CPEntriesContent=$( cat $CPEntriesDir)
+                    preparedCPs=$( python scripts/python/reassemble-cps.py $CPEntriesContent "$project")
+                    echo "Compiled test $oldTestClass"
+                    javac -cp "$preparedCPs:$resultDir:$(cat libs/test_execution/classpath.txt)" $oldTestClass
+                else
+                    echo "did not compile the sub class: "
+                fi
+                oldTestClass=$failedClass
+            fi
+
+            counter=$((counter+1))
+            echo "Counter: $counter"
+        done
+
+        mainRegTest="$resultDir/RegressionTest.java"
+        echo "Compiling the main Regression test: $mainRegTest"
+        javac -cp "$preparedCPs:$resultDir:$(cat libs/test_execution/classpath.txt)" $mainRegTest
+
+    else
+        failedClass=$(python scripts/python/mutation/detect_failing_class.py "$logFile" "did not pass without mutation")
+        failedTest=$(python scripts/python/mutation/detect_failing_test.py "$logFile" "did not pass without mutation")
+
+        # Add @Ignore to failing tests
+        for mainTest in `find $resultDir -name "*_ESTest.java" -type f`; do
+            echo "Add ignore to $failedTest in $mainTest"
+            java -jar tools/IgnoreAdder.jar $mainTest "$failedTest"
+            CPEntriesDir="projects/$project/cp-entries.txt"
+            CPEntriesContent=$( cat $CPEntriesDir)
+            preparedCPs=$( python scripts/python/reassemble-cps.py $CPEntriesContent "$project")
+            javac -cp "$preparedCPs:$resultDir:$(cat libs/test_execution/classpath.txt)" $mainTest
+        done
+    fi
 
     echo "Problem has been fixed."
     echo "Rerunning the PIT execution ..."
---
+++ scripts/python/mutation/detect_failing_test_randoop.py
@@ -0,0 +1,16 @@
+import sys
+
+fileDir=sys.argv[1]
+pattern=sys.argv[2]
+caseNumber=sys.argv[3]
+
+counter=0
+for line in open(fileDir, "r"):
+    if "did not pass without mutation" in line:
+        counter+=1
+        if counter == int(caseNumber):
+            testName=line.split(", name=")[1].split("(")[0]
+            print testName
+            break
+
+# Description [testClass=RegressionTest65, name=test13057(RegressionTest65)]
